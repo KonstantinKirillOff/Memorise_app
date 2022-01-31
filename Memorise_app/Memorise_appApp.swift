60c181688477fb185244d3f64abfbe1927568416
kirillOff <kirilloffkostya@gmail.com>
2022-01-31 09:38:33 +0300

    Initial Commit

---
+++ Memorise_app/Memorise_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorise_appApp.swift
+//  Memorise_app
+//
+//  Created by Konstantin Kirillov on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Memorise_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
